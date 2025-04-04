8594c6d555dd795bb749ba3cfd30fe2bc7fbc703
CodingManoj <CodingManoj@gmail.com>
2025-04-03 20:50:32 -0400

    Terraform - Expense Automation : Auto Push

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "expense" {
-  source = "git::https://github.com/B59-CloudDevOps/expense-terraform.git"
+  source = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
   env    = var.env
 }
