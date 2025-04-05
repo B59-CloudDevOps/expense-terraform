155b7e054963bc1f70f818f1ccd969b903aabe81
CodingManoj <CodingManoj@gmail.com>
2025-04-04 21:14:21 -0400

    Terraform - Expense Automation : Auto Push

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "frontend" {
   source         = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
   env            = var.env
   zone_id        = var.zone_id
-  intance_type   = var.components["frontend"]["instance_type"]
+  instance_type  = var.components["frontend"]["instance_type"]
   name           = var.components["frontend"]["name"]
   port_no        = var.components["frontend"]["port_no"]
   port_no_of_ssh = var.components["frontend"]["port_no_of_ssh"]
@@ -14,7 +14,7 @@ module "backend" {
   source         = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
   env            = var.env
   zone_id        = var.zone_id
-  intance_type   = var.components["backend"]["instance_type"]
+  instance_type  = var.components["backend"]["instance_type"]
   name           = var.components["backend"]["name"]
   port_no        = var.components["backend"]["port_no"]
   port_no_of_ssh = var.components["backend"]["port_no_of_ssh"]
@@ -24,7 +24,7 @@ module "mysql" {
   source         = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
   env            = var.env
   zone_id        = var.zone_id
-  intance_type   = var.components["mysql"]["instance_type"]
+  instance_type  = var.components["mysql"]["instance_type"]
   name           = var.components["mysql"]["name"]
   port_no        = var.components["mysql"]["port_no"]
   port_no_of_ssh = var.components["mysql"]["port_no_of_ssh"]
