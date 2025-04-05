137e6180dac89dacbca1c63f49d298a444accc58
CodingManoj <CodingManoj@gmail.com>
2025-04-04 21:06:15 -0400

    Terraform - Expense Automation : Auto Push

--- main.tf
+++ main.tf
@@ -1,29 +1,34 @@
-module "mysql" {
+module "frontend" {
+  depends_on     = [module.backend]
   source         = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
   env            = var.env
-  intance_type   = ""
-  zone_id        = ""
-  name           = ""
-  port_no        = ""
-  port_no_of_ssh = ""
+  zone_id        = var.zone_id
+  intance_type   = var.components["frontend"]["instance_type"]
+  name           = var.components["frontend"]["name"]
+  port_no        = var.components["frontend"]["port_no"]
+  port_no_of_ssh = var.components["frontend"]["port_no_of_ssh"]
 }
 
 module "backend" {
+  depends_on     = [module.mysql]
   source         = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
   env            = var.env
-  intance_type   = ""
-  zone_id        = ""
-  name           = ""
-  port_no        = ""
-  port_no_of_ssh = ""
+  zone_id        = var.zone_id
+  intance_type   = var.components["backend"]["instance_type"]
+  name           = var.components["backend"]["name"]
+  port_no        = var.components["backend"]["port_no"]
+  port_no_of_ssh = var.components["backend"]["port_no_of_ssh"]
 }
 
-module "frontend" {
+module "mysql" {
   source         = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
   env            = var.env
-  intance_type   = ""
-  zone_id        = ""
-  name           = ""
-  port_no        = ""
-  port_no_of_ssh = ""
+  zone_id        = var.zone_id
+  intance_type   = var.components["mysql"]["instance_type"]
+  name           = var.components["mysql"]["name"]
+  port_no        = var.components["mysql"]["port_no"]
+  port_no_of_ssh = var.components["mysql"]["port_no_of_ssh"]
 }
+
+
+
--- vars.tf
+++ vars.tf
@@ -1,7 +1,3 @@
 variable "env" {}
 variable "zone_id" {}
-# variable "instance_type" {}
-# variable "name" {}
-
-# variable "port_no" {}
-# variable "port_no_of_ssh" {}
+variable "components" {}
