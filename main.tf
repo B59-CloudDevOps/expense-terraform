fdad825ba5322f7d5e284dfcef7d9d5bd58abdd7
CodingManoj <CodingManoj@gmail.com>
2025-04-04 20:51:36 -0400

    Terraform - Expense Automation : Auto Push

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 module "frontend" {
-  source       = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
-  env          = var.env
-  intance_type = ""
-  name         = ""
+  source         = "git::https://github.com/B59-CloudDevOps/tf-module-app.git"
+  env            = var.env
+  intance_type   = ""
+  zone_id        = ""
+  name           = ""
+  port_no        = ""
+  port_no_of_ssh = ""
+
 }
 
 
